0ff235d9840c47f4be08d949e5e78598fcaf985c
JanLewDev <lewandowski.jan@o2.pl>
2023-12-05 16:25:52 +0100

    5th of December

--- 4.py
+++ 4.py
@@ -1,6 +1,7 @@
 with open("in1") as f:
     data = f.read()
 
+from collections import defaultdict
 
 def integers(string):
     numbers = [int(x) for x in string.split() if x.isnumeric()]
@@ -13,6 +14,7 @@ games = [line.strip().split(": ") for line in lines]
 
 cards = [1 for _ in range(len(games) + 1)]
 
+d = defaultdict(lambda: 1)
 
 for game in games:
     idx = integers(game[0])[0]
@@ -25,11 +27,12 @@ for game in games:
             score += 1
 
     for i in range(idx+1, idx + score + 1):
-            cards[i] += cards[idx]
+        cards[i] += cards[idx]
+        d[i] += d[idx]
 
     # if score:
     #     ans += 2 ** (score - 1)
     
-    ans += cards[idx]
+    ans += d[idx]
 
 print(f"answer is {ans}")
